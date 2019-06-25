86d556c5c69fb596b7ae99b3e77f3ec8dfd6d90e
grahamcwycyshyn <grahamcwycyshyn@gmail.com>
2019-06-25 15:53:47 -0400

    initial commit

---
+++ Lab4.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Lab4 {
+public static void main(String[] args) {
+	
+	Scanner input = new Scanner(System.in);
+	
+	char answer = 'y';
+	
+	//Takes user integer input, uses three for loops to print the lists of numbers, squares and cubes
+	do {
+		
+		System.out.println("Enter an integer:");
+		
+		int integer = input.nextInt();
+		
+		System.out.printf("%-10s %-10s %-10s\n", "Numbers:", "Squared:", "Cubed:");
+		
+		System.out.printf("%-10s %-10s %-10s\n", "=======", "=======", "=======");
+		
+		for(int i = 1; i <= integer; i++) {
+			System.out.printf("%-10s %-10s %-10s\n", i, i * i, i * i * i);
+		}
+		
+		System.out.println("Would you like to continue? (y/n)");
+		
+		answer = input.next().charAt(0);
+		
+	} while(answer == 'y' || answer == 'Y');
+	//Will only continue if user enters y or Y
+
+	input.close();
+	
+	}
+
+//nearHundred solution
+public boolean nearHundred(int n) {
+	  if(Math.abs(100-n) <= 10 || Math.abs(200-n) <= 10){
+	    return true;}
+	  else{
+	    return false;}
+	  }
+}
