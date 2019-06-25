7498f46709e4460b1103d4fd5662d3991fb001eb
grahamcwycyshyn <grahamcwycyshyn@gmail.com>
2019-06-25 16:32:24 -0400

    added multiplication table

--- Lab4.java
+++ Lab4.java
@@ -1,37 +1,42 @@
 import java.util.Scanner;
 
 public class Lab4 {
-public static void main(String[] args) {
 	
-	Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		char answer = 'y';
+		
+		//Takes user integer input, uses three for loops to print the lists of numbers, squares and cubes
+		do {
+			
+			System.out.println("Enter an integer:");
+			
+			int integer = input.nextInt();
+			
+			System.out.printf("%-10s %-10s %-10s\n", "Numbers:", "Squared:", "Cubed:");
+			
+			System.out.printf("%-10s %-10s %-10s\n", "=======", "=======", "=======");
+			
+			for(int i = 1; i <= integer; i++) {
+				System.out.printf("%-10s %-10s %-10s\n", i, i * i, i * i * i);
+			}
+			
+			System.out.println();
+			
+			printTable(integer);
+			
+			System.out.println("Would you like to continue? (y/n)");
+			
+			answer = input.next().charAt(0);
+			
+		} while(answer == 'y' || answer == 'Y');
+		//Will only continue if user enters y or Y
 	
-	char answer = 'y';
-	
-	//Takes user integer input, uses three for loops to print the lists of numbers, squares and cubes
-	do {
+		input.close();
 		
-		System.out.println("Enter an integer:");
-		
-		int integer = input.nextInt();
-		
-		System.out.printf("%-10s %-10s %-10s\n", "Numbers:", "Squared:", "Cubed:");
-		
-		System.out.printf("%-10s %-10s %-10s\n", "=======", "=======", "=======");
-		
-		for(int i = 1; i <= integer; i++) {
-			System.out.printf("%-10s %-10s %-10s\n", i, i * i, i * i * i);
 		}
-		
-		System.out.println("Would you like to continue? (y/n)");
-		
-		answer = input.next().charAt(0);
-		
-	} while(answer == 'y' || answer == 'Y');
-	//Will only continue if user enters y or Y
-
-	input.close();
-	
-	}
 
 //nearHundred solution
 public boolean nearHundred(int n) {
@@ -40,4 +45,27 @@ public boolean nearHundred(int n) {
 	  else{
 	    return false;}
 	  }
+
+//multiplication table print method
+public static void printTable(int integer) {
+	
+	  System.out.format("      ");
+	   
+       for(int i = 1; i<=integer; i++ ) {
+           System.out.format("%4d",i);
+       }
+       
+       System.out.println();
+      
+       for(int i = 1; i <= integer; i++) {
+           System.out.format("%4d  ", i);
+           
+           for(int j = 1;j <= integer;j++) {
+               System.out.format("%4d",i*j);
+           }
+           
+           System.out.println();
+       }
+   }
 }
+
